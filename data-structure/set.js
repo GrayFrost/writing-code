eb9f9891400d844678cc52076030f9f43ec216b9
zzh <1760655799@qq.com>
2020-02-26 23:31:42 +0800

    feat(Set): Set完善

--- data-structure/set.js
+++ data-structure/set.js
@@ -1,21 +1,45 @@
 class Set {
- constructor(){
-   this.items = {}
- }
- add(val){
-   if(!this.has(val)){
-     this.items[val] = val;
-   }
- }
- has(val){
-   return this.items.hasOwnProperty(val);
- }
- remove(val){
-   if(this.has(val)){
-     delete this.items[val];
-   }
- }
- get size(){
-   return Object.keys(this.items).length;
- }
+  constructor() {
+    this.items = {};
+  }
+  has(value) {
+    return this.items.hasOwnProperty(value);
+  }
+  add(value) {
+    if (!this.has(value)) {
+      this.items[value] = value;
+      return true;
+    }
+    return false;
+  }
+  remove(value) {
+    if (this.has(value)) {
+      delete this.items[value];
+      return true;
+    }
+    return false;
+  }
+  get size() {
+    return Object.keys(this.items).length;
+  }
+  get values() {
+    return Object.keys(this.items);
+  }
+  // 并集
+  union(otherSet) {
+    const unionSet = new Set();
+    this.values.forEach(value => unionSet.add(value));
+    otherSet.forEach(value => unionSet.add(value));
+    return unionSet;
+  }
+  // 交集
+  intesection(otherSet) {
+    const intersedtionSet = new Set();
+    this.values.forEach(value => {
+      if (otherSet.has(value)) {
+        intersedtionSet.add(value);
+      }
+    });
+    return intersedtionSet;
+  }
 }
