dd4a16cfeb8626aafe67996c90f1f685034c3869
zzh <1760655799@qq.com>
2020-02-27 00:18:20 +0800

    feat(LinkedList): 链表完善

--- data-structure/linkedList.js
+++ data-structure/linkedList.js
@@ -1,60 +1,104 @@
 class Node {
-  constructor(val){
+  constructor(val) {
     this.val = val;
     this.next = null;
   }
 }
 
 class LinkedList {
-  constructor(){
+  constructor() {
     this.head = null;
     this.length = 0;
   }
-  append(val){
+  append(val) {
     const node = new Node(val);
-    if(this.head === null){
+    if (this.head === null) {
       this.head = node;
-    }else{
+    } else {
       let current = this.head;
-      while(current){
+      while (current.next) {
         current = current.next;
       }
       current.next = node;
     }
     this.length++;
   }
-  insert(position, val){
-
-  }
-  findIndex(val){
-
-  }
-  remove(val){
-    const position = this.findIndex(val);
-    this.removeAt(position, val);
-  }
-  removeAt(position, val){
-
-
-    this.length--;
-  }
-  get size(){
-    return this.length;
-  }
-  isEmpty(){
-    return this.length === 0;
-  }
-  print(){
-    let str;
-    if(this.head === null){
-      return '';
-    }else{
-      let current = this.head;
-      while(current){
-        str = str + '' + current.val;
-        current = current.next;
+  insert(position, val) {
+    if (position > -1 && position < this.length - 1) {
+      let node = new Node(val);
+      let index = 0;
+      let prev, current;
+      if (position === 0) {
+        current = this.head;
+        this.head = node;
+        this.head.next = current;
+      } else {
+        current = this.head;
+        while (index < position) {
+          prev = current;
+          current = current.next;
+          index++;
+        }
+        prev.next = node;
+        node.next = current;
       }
-      return str;
+      this.length++;
     }
   }
+  findIndex(val) {
+    let index = 0;
+    let current = this.head;
+    while (index < this.length) {
+      if (current.val === val) {
+        return index;
+      }
+      current = current.next;
+      index++;
+    }
+    return -1;
+  }
+  remove(val) {
+    const position = this.findIndex(val);
+    this.removeAt(position);
+  }
+  removeAt(position) {
+    if (position > -1 && position < this.length - 1) {
+      let index = 0;
+      let prev, current;
+      if (index === 0) {
+        this.head = this.head.next;
+      } else {
+        current = this.head;
+        while (index < position) {
+          prev = current;
+          current = current.next;
+          index++;
+        }
+        prev.next = current.next;
+      }
+      this.length--;
+    }
+  }
+  get size() {
+    return this.length;
+  }
+  isEmpty() {
+    return this.length === 0;
+  }
+  print() {
+    let current = this.head;
+    let str = current.val;
+    while (current.next) {
+      current = current.next;
+      str = str + " " + current.val;
+    }
+    console.log(str);
+  }
 }
+
+let linkedList = new LinkedList();
+linkedList.append(5);
+linkedList.append(10);
+linkedList.append(15);
+linkedList.append(20);
+linkedList.print();
