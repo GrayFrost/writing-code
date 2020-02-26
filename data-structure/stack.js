4b87d177d1d0628aacf73c4f6d6497aea45e4120
zzh <1760655799@qq.com>
2020-02-26 23:35:02 +0800

    feat(Stack): Stack完善
    目前stack是自己写的最好的

--- data-structure/stack.js
+++ data-structure/stack.js
@@ -1,60 +1,63 @@
 class Stack {
-  constructor(){
+  constructor() {
     this.items = [];
   }
-  pop(){
+  pop() {
     return this.items.pop();
   }
-  push(item){
+  push(item) {
     this.items.push(item);
   }
-  isEmpty(){
+  isEmpty() {
     return this.items.length === 0;
   }
-  get size(){
+  get size() {
     return this.items.length;
+  }
+  clear() {
+    this.items = [];
   }
 }
 
 // 应用
 // 十进制转二进制
-function convert(num){
+function convert(num) {
   const stack = new Stack();
   let remainder;
-  while(num>0){
+  while (num > 0) {
     remainder = num % 2;
     stack.push(remainder);
     num = Math.floor(num / 2);
   }
-  let str = '';
-  while(!stack.isEmpty()){
+  let str = "";
+  while (!stack.isEmpty()) {
     str += stack.pop();
   }
   return str;
 }
 const str = convert(7);
-console.log('转化的二进制数为', str); // 111
+console.log("转化的二进制数为", str); // 111
 
 // 括号匹配
-function isMatch(str){
+function isMatch(str) {
   const map = {
-    '(': ')',
-    '[': ']',
-    '{': '}'
-  }
+    "(": ")",
+    "[": "]",
+    "{": "}"
+  };
   const stack = new Stack();
-  for(let i = 0, length = str.length; i < length; i++){
+  for (let i = 0, length = str.length; i < length; i++) {
     let char = str[i];
-    if(char in  map){
+    if (char in map) {
       stack.push(char);
-    }else if(map[stack.pop()] !== char){
+    } else if (map[stack.pop()] !== char) {
       return false;
     }
   }
-  
+
   return !stack.length; // 最后判断，如果栈为空，则匹配
 }
 
-const str2 = '([{}]{})';
+const str2 = "([{}]{})";
 const match = isMatch(str2);
-console.log('匹配吗', match);
+console.log("匹配吗", match);
