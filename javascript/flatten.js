e7a7835e8da88617b0cd44506c7b20405b455f0e
zzh <1760655799@qq.com>
2020-02-27 23:57:30 +0800

    add(flatten): add new file

---
+++ javascript/flatten.js
@@ -0,0 +1,21 @@
+function flatten(arr) {
+  return arr
+    .toString()
+    .split(",")
+    .map(item => +item);
+}
+
+function flatten2(arr, depth = 1) {
+  if (depth < 1) {
+    return arr;
+  }
+  if (depth === 1) {
+    return arr.reduce((prev, cur) => {
+      return prev.concat(cur);
+    }, []);
+  } else {
+    return arr.reduce((prev, cur) => {
+      return prev.concat(Array.isArray(cur) ? flatten2(cur, depth - 1) : cur);
+    }, []);
+  }
+}
