7a76e02addc1747708f5b3993d3afb5ed19a1932
zzh <1760655799@qq.com>
2020-02-28 00:21:46 +0800

    feat(throttle): add new content

--- javascript/throttle.js
+++ javascript/throttle.js
@@ -1,3 +1,12 @@
-function throttle(fn){
-  
+function throttle(fn, ms) {
+  let timeout;
+  return function() {
+    let args = arguments;
+    if (!timeout) {
+      timeout = setTimeout(() => {
+        fn.apply(this, args);
+        timeout = null;
+      }, ms * 1000);
+    }
+  };
 }
