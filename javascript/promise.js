bb6c800e442c958758b580aa1dd10c7e81dbea7e
zzh <1760655799@qq.com>
2020-02-28 00:19:44 +0800

    WIP(promise): add new content

--- javascript/promise.js
+++ javascript/promise.js
@@ -1,23 +1,75 @@
-const PENDING = 'pending';
-const FULFILLED = 'fulfilled';
-const REJECTED = 'rejected';
+const PENDING = "pending";
+const FULFILLED = "fulfilled";
+const REJECTED = "rejected";
 
 class MyPromise {
-  constructor(fn){
+  constructor(fn) {
     this.status = PENDING;
     this.value = undefined;
     this.reason = undefined;
+    this.onResolvedCallback = [];
+    this.onRejectedCallback = [];
 
-    function resolve(value){
-      this.status = FULFILLED;
-      this.value = value;
+    function resolve(value) {
+      if (this.status === PENDING) {
+        this.status = FULFILLED;
+        this.value = value;
+        this.onResolvedCallback.forEach(fn => {
+          fn();
+        });
+      }
     }
-    function reject(reason){
-      this.status = REJECTED;
-      this.reason = reason;
+    function reject(reason) {
+      if (this.status === REJECTED) {
+        this.status = REJECTED;
+        this.reason = reason;
+        this.onRejectedCallback.forEach(fn => {
+          fn();
+        });
+      }
     }
 
-    fn(resolve, reject);
+    try {
+      fn(resolve, reject);
+    } catch (e) {
+      reject(e);
+    }
   }
-  then(){}
+  then(onFulfilled, onRejected) {
+    let promise2 = new MyPromise((resolve, reject) => {
+      if (this.status === FULFILLED) {
+        onFulfilled(this.value);
+      }
+      if (this.status === REJECTED) {
+        onRejected(this.reason);
+      }
+      if (this.status === PENDING) {
+        this.onResolvedCallback.push(() => {
+          onFulfilled(this.value);
+        });
+        this.onRejectedCallback.push(() => {
+          onRejected(this.reason);
+        });
+      }
+    });
+    return promise2;
+  }
 }
+
+MyPromise.resolve = function(value) {
+  return new MyPromise((resolve, reject) => {
+    resolve(value);
+  });
+};
+MyPromise.reject = function(error) {
+  return new MyPromise((resolve, reject) => {
+    reject(error);
+  });
+};
+MyPromise.race = function(promises) {
+  return new MyPromise((resolve, reject) => {
+    promises.forEach(promise => {
+      resolve(promise);
+    });
+  });
+};
