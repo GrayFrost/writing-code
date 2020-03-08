4f5dfa1978f6286e84cbb8d03e0a7e21dfb54b9f
zzh <1760655799@qq.com>
2020-03-08 17:29:33 +0800

    feat(event-emitter): add new content

--- javascript/eventEmitter.js
+++ javascript/eventEmitter.js
@@ -1,25 +1,34 @@
 class Event {
-  constructor(){
-    this.events = {}
+  constructor() {
+    this.events = {};
   }
-  on(event, cb){
-    if(!this.events[event]){
+  on(event, cb) {
+    if (!this.events[event]) {
       this.events[event] = [];
     }
     this.events[event].push(cb);
   }
-  emit(event, ...args){
-    if(!this.events[event]){
+  emit(event, ...args) {
+    if (!this.events[event]) {
       return;
     }
     this.events[event].forEach((fn, index) => {
       fn.call(this, ...args);
-    })
+    });
   }
-  off(event, fn){
-    // TODO: 
+  off(event, fn) {
+    if (!this.events[event]) {
+      return;
+    }
+    this.events[event] = this.events[event].filter(item => {
+      return item !== fn;
+    });
   }
-  once(event, fn){
-    
+  once(event, fn) {
+    const only = () => {
+      fn.apply(this, arguments);
+      this.off(event, only);
+    };
+    this.on(event, only);
   }
 }
