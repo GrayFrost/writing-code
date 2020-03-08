a03b513d0fa1bb0cfbbde647c4d97478b831d3d7
zzh <1760655799@qq.com>
2020-03-08 17:21:16 +0800

    feat(bind): add new content

--- javascript/bind.js
+++ javascript/bind.js
@@ -1,6 +1,11 @@
-Function.prototype.myBind = function(context){
+Function.prototype.myBind = function(context) {
   let obj = context || window;
   let arg1 = Array.prototype.slice.call(arguments, 1);
   let fn = this;
-  
-}
+  let fBound = function() {
+    let arg2 = Array.prototype.slice.call(arguments);
+    return fn.apply(this instanceof fBound ? this : obj, arg1.concat(arg2));
+  };
+  fBound.prototype = this.prototype;
+  return fBound;
+};
