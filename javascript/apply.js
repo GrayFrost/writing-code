13538162d948a67bcd436f5b82ba3adc63d140ef
zzh <1760655799@qq.com>
2020-03-08 17:21:05 +0800

    style(apply): code format

--- javascript/apply.js
+++ javascript/apply.js
@@ -1,16 +1,16 @@
-Function.prototype.myApply = function(context, args){
+Function.prototype.myApply = function(context, args) {
   const obj = context || window;
   const fn = Symbol();
   obj[fn] = this;
   const result = obj[fn](...args);
   delete obj[fn];
   return result;
-}
+};
 
-function Person(age, gender){
-  console.log(this.name, age, gender)
+function Person(age, gender) {
+  console.log(this.name, age, gender);
 }
 const person = {
-  name: 'gary'
-}
-Person.myApply(person, [26, 'male']);
+  name: "gary"
+};
+Person.myApply(person, [26, "male"]);
