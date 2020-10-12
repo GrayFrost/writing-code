2b41d765fa32de1c53bbb88e2cb25c5edae174bb
zzh <1760655799@qq.com>
2020-10-12 18:20:00 +0800

    feat(排序): 归并排序

--- sort/merge.js
+++ sort/merge.js
@@ -1,6 +1,27 @@
 // 归并排序
-function mergeSort(arr){
-
+function mergeSort(arr) {
+    const length = arr.length;
+    if (length < 2) {
+        return arr;
+    }
+    let mid = Math.floor(length / 2);
+    let left = arr.slice(0, mid);
+    let right = arr.slice(mid);
+    return merge(mergeSort(left), mergeSort(right));
 }
 
-function merge(){}
+function merge(left, right) {
+    const result = [];
+    while (left.length && right.length) {
+        if (left[0] <= right[0]) {
+            result.push(left.shift());
+        } else {
+            result.push(right.shift());
+        }
+    }
+    const newResult = result.concat(left.length ? left : right);
+    return newResult;
+}
+
+const arr = [1, 4, 5, 6, 2, 3, 7, 9, 8];
+console.log("merge", mergeSort(arr));
