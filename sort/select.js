6942f27956ea15f63bb2ba466ab77dc401064633
zzh <1760655799@qq.com>
2020-10-12 16:36:09 +0800

    feat(排序): 选择排序

--- sort/select.js
+++ sort/select.js
@@ -1,4 +1,23 @@
 // 选择排序
-function selectSort(arr){
-  
+function swap(i, j, array) {
+    const temp = array[j];
+    array[j] = array[i];
+    array[i] = temp;
 }
+
+function selectSort(arr) {
+    const length = arr.length;
+    for (let i = 0; i < length; i++) {
+        let min = i;
+        for (let j = i + 1; j < length; j++) {
+            if (arr[j] < arr[min]) {
+                min = j;
+            }
+        }
+        swap(i, min, arr);
+    }
+    return arr;
+}
+
+const arr = [1, 4, 5, 6, 2, 3, 7, 9, 8];
+console.log("select", selectSort(arr));
