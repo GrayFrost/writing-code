949127d34d6786f66ec600dfa95ff01e497137df
zzh <1760655799@qq.com>
2020-10-17 17:18:48 +0800

    feat(排序): 快速排序

--- sort/quick.js
+++ sort/quick.js
@@ -1,4 +1,21 @@
 // 快速排序
-function quickSort(arr){
-  
+function quickSort(arr) {
+    let length = arr.length;
+    if (length < 2) {
+        return arr;
+    }
+    const pivot = Math.floor(Math.random() * length);
+    let minArr = [],
+        maxArr = [];
+    for (let i = 0; i < length; i++) {
+        if (arr[i] < arr[pivot] && i !== pivot) {
+            minArr.push(arr[i]);
+        } else if (arr[i] >= pivot && i !== pivot) {
+            maxArr.push(arr[i]);
+        }
+    }
+    return [...quickSort(minArr), arr[pivot], ...quickSort(maxArr)];
 }
+
+const arr = [1, 4, 5, 6, 2, 3, 7, 9, 8];
+console.log("quick 1", quickSort(arr));
